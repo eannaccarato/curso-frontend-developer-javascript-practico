5b0098e6914152b39e47f74ec57da6fa34d53e8b
Esequiel Annaccarato <eannaccarato@gmail.com>
2022-12-20 15:43:15 -0300

    Terminadas todas las interacciones entre menus y productos, sigue el Pull Request (PR)

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 const menuEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
+const productDetailCloseIcon = document.querySelector('.product-detail-close')
 const menuHamIcon = document.querySelector('.menu')
 const mobileMenu = document.querySelector('.mobile-menu')
 const shoppingCartContainer = document.querySelector('#shoppingCartContainer')
+const productDetailContainer = document.querySelector('#productDetail')
 const cardsContainer = document.querySelector('.cards-container')
 
 menuEmail.addEventListener('click', toggleDesktopMenu)
 menuHamIcon.addEventListener('click', toggleMobileMenu)
 menuCarritoIcon.addEventListener('click', toggleCarritoAside)
+productDetailCloseIcon.addEventListener('click', closeProductDetailAside)
 
 function toggleDesktopMenu() {
 const isAsideClosed = shoppingCartContainer.classList.contains('inactive')
@@ -27,9 +30,10 @@ if (!isAsideClosed){
     shoppingCartContainer.classList.add('inactive')
 }
 
+    closeProductDetailAside()
+    
     mobileMenu.classList.toggle('inactive')
 }
-
 function toggleCarritoAside() {
 const isMobileMenuClosed = mobileMenu.classList.contains('inactive')
 
@@ -37,8 +41,21 @@ if (!isMobileMenuClosed){
     mobileMenu.classList.add('inactive')
 }
 
+const isProductDetailClosed = productDetailContainer.classList.contains('inactive')
+
+if (!isProductDetailClosed){
+    productDetailContainer.classList.add('inactive')
+}
+
     shoppingCartContainer.classList.toggle('inactive')
     
+}
+function openProductDetailAside() {
+    shoppingCartContainer.classList.add('inactive')
+    productDetailContainer.classList.remove('inactive')
+}
+function closeProductDetailAside() {
+    productDetailContainer.classList.add('inactive')
 }
 
 const productList = [];
@@ -80,6 +97,7 @@ function renderProducts(arr) {
     
         const productImg = document.createElement('img')
         productImg.setAttribute('src', product.image)
+        productImg.addEventListener('click', openProductDetailAside)
     
         const productInfo = document.createElement('div')
         productInfo.classList.add('product-info')
